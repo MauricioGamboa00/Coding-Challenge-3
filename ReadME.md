46cb5cc700438f1c6be0a1ce027b5ea031729834
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-01 13:20:25 -0500

    Task 1 - Product Price Management

---
+++ ReadME.md
@@ -0,0 +1,2 @@
+# Coding Challenge 3
+My submission for coding challenge 3
---
+++ cc_3.js
@@ -0,0 +1,7 @@
+// Task 1 Product Price Management Scenario
+
+let prices = [10, 20, 15, 30, 40]; // Created an Array for product prices
+prices.push(25); // Added a new price to the array
+prices.shift(); // Removed first price from the array
+
+console.log("Updated Price List:", prices); // Log the updated price list in the console
