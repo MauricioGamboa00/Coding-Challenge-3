2e8107b67c02260dac998e12160d1b243c0c6df8
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-01 13:32:51 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,12 @@ prices.push(25); // Added a new price to the array
 prices.shift(); // Removed first price from the array
 
 console.log("Updated Price List:", prices); // Log the updated price list in the console
+
+// Task 2 Modifying Customer Orders
+
+let orders = [200, 400, 600, 800, 1000]; // Created an array for order quantities 
+orders[2] +=5; // Increased third order by 5
+let totalOrders = orders.reduce((total, orders) => total + orders); // Calculate total number of all orders
+
+console.log("Updated Orders List:", orders); // Log updated orders list to console
+console.log("Total Order Amount:", totalOrders); // Log the total number of all orders to console
